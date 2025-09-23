9c3cfc7b34af45d9c9125daaeff2293c5a23a61d
codewithna <codewithna73@gmail.com>
2025-09-23 20:38:27 +0530

    new file add and googel form code add

---
+++ googleform.js
@@ -0,0 +1,6 @@
+document.getElementById("googleForm-btn").addEventListener("click", function() {
+  window.open(
+    "https://docs.google.com/forms/d/e/1FAIpQLSe5evohqC27f4j8stIVnerubEf06Ufz900sKY2QU_vr-BKb0Q/viewform",
+    "_blank"
+  );
+});
